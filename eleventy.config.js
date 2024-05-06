fa0e5ddf22fe519299cbfec405fed3f7d8b49f17
Ndimah Mike <ndimah22@protonmail.com>
2024-05-06 16:00:01 +0100

    feat(config): setup eleventy in the project

---
+++ eleventy.config.js
@@ -0,0 +1,10 @@
+module.exports = function (config) {
+    config.addPassthroughCopy("assets");
+    return {
+        dir: {
+            input: "src",
+            output: "_site",
+            includes: "_templates"
+        }
+    };
+};
