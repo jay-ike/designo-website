74167d57be525cd6d1c257d160621968737b65a2
Ndimah Mike <ndimah22@protonmail.com>
2024-05-08 16:08:23 +0100

    feat(setup): organize site datas

---
+++ src/_data/site.js
@@ -0,0 +1,25 @@
+
+module.exports = Object.freeze({
+    links: [
+        {title: "our company", path: ""},
+        {title: "locations", path: ""},
+        {title: "contact", path: ""}
+    ],
+    services: [
+        {
+            "link": "",
+            "title": "web design",
+            "topic": "web"
+        },
+        {
+            "link": "",
+            "title": "app design",
+            "topic": "app"
+        },
+        {
+            "link": "",
+            "title": "graphic design",
+            "topic": "graphic"
+        }
+    ]
+});
