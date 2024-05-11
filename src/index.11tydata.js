2d8bd760ccbc1dd4f83fbfe8d4d23db1df26def0
Ndimah Mike <ndimah22@protonmail.com>
2024-05-11 16:02:06 +0100

    feat(config): add style in the project

--- src/index.11tydata.js
+++ src/index.11tydata.js
@@ -2,7 +2,6 @@ const {getFilePaths} = require("../utils");
 
 async function getHero(data) {
     const images = await getFilePaths("src" + data.page.url);
-    console.log("images: ", images);
     return images[0];
 }
 
@@ -12,19 +11,19 @@ module.exports = Object.freeze({
     },
     features: [
         {
-            alt: "",
+            alt: "illustration of a designer enjoying sketching user interfaces",
             description: "Each project starts with an in-depth brand research to ensure we only create products that serve a purpose. We merge art, design, and technology into exciting new solutions.",
             tag: "passion",
             title: "Passionate"
         },
         {
-            alt: "",
+            alt: "illustration of an engineer looking at plans in the wall while designing an interface with his tablet",
             description: "Everything that we do has a strategic purpose. We use an agile approach in all of our projects and value customer collaboration. It guarantees superior results that fulfill our clients’ needs.",
             tag: "resource",
             title: "Resourceful"
         },
         {
-            alt: "",
+            alt: "illustration of a man and a woman sharing design insight with smile",
             description: "We are a group of enthusiastic folks who know how to put people first. Our success depends on our customers, and we strive to give them the best experience a company can provide.",
             tag: "friendship",
             title: "Friendly"
