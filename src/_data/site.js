773f97a503381189a5584c5413bd822c7d6756c0
Ndimah Mike <ndimah22@protonmail.com>
2024-05-14 17:49:19 +0100

    feat(styling): finish the home page layout implementation

--- src/_data/site.js
+++ src/_data/site.js
@@ -5,5 +5,10 @@ module.exports = Object.freeze({
         { title: "locations", path: "" },
         { title: "contact", path: "" }
     ],
+    socials: [
+        { title: "github account", tag: "github", link: ""},
+        { title: "mastodon account", tag: "mastodon", link: ""},
+        { title: "X formerly twitter account", tag: "twitter", link: ""}
+    ]
 });
 
