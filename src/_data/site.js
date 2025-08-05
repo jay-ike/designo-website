1d5afcf4f7a77878b2d42f992f793eff05c23736
Ndimah Tchougoua <ndimah22@protonmail.com>
2025-08-05 14:34:42 +0100

    feat: add the not by AI banner

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,14 +1,14 @@
 
 module.exports = Object.freeze({
     links: [
-        { title: "our company", path: "/company/about" },
-        { title: "locations", path: "/company/locations" },
-        { title: "contact", path: "/company/contact" }
+        {title: "our company", path: "/company/about"},
+        {title: "locations", path: "/company/locations"},
+        {title: "contact", path: "/company/contact"}
     ],
     socials: [
-        { title: "github account", tag: "github", link: "https://github.com/jay-ike"},
-        { title: "mastodon account", tag: "mastodon", link: "https://mastodon.social/@ndimah22"},
-        { title: "X formerly twitter account", tag: "twitter", link: "https://twitter.com/jospinEvans"}
+        {title: "github account", tag: "github", link: "https://github.com/jay-ike"},
+        {title: "mastodon account", tag: "mastodon", link: "https://mastodon.social/@ndimah22"},
+        {title: "X formerly twitter account", tag: "twitter", link: "https://twitter.com/jospinEvans"}
     ]
 });
 
