7013608df03a874ebf8352086c2b60110db05307
Ndimah Tchougoua <ndimah22@protonmail.com>
2026-09-10 19:11:58 +0100

    fix: update the opengraph images for each page

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,14 +1,39 @@
 
 module.exports = Object.freeze({
+    image: {
+        alt: "a screenshot of the designo digital agency home page",
+        description: "With over 10 years in the industry, we are " +
+        "experienced in creating fully responsive websites, app design, " +
+        "and engaging brand experiences. Find out more about our services.",
+        height: 614,
+        name: "Designo Digital Agency",
+        src: "https://ike-designo-website.vercel.app/assets/images/" +
+        "screenshots/home-page.jpg",
+        title: "Designo a digital agency for your digital needs",
+        url: "https://ike-designo-website.vercel.app/",
+        width: 1024
+    },
     links: [
         {title: "our company", path: "/company/about"},
         {title: "locations", path: "/company/locations"},
         {title: "contact", path: "/company/contact"}
     ],
     socials: [
-        {title: "github account", tag: "github", link: "https://github.com/jay-ike"},
-        {title: "mastodon account", tag: "mastodon", link: "https://mastodon.social/@ndimah22"},
-        {title: "X formerly twitter account", tag: "twitter", link: "https://twitter.com/jospinEvans"}
+        {
+            title: "github account",
+            tag: "github",
+            link: "https://github.com/jay-ike"
+        },
+        {
+            title: "mastodon account",
+            tag: "mastodon",
+            link: "https://mastodon.social/@ndimah22"
+        },
+        {
+            title: "X (formerly twitter) account",
+            tag: "twitter",
+            link: "https://twitter.com/jospinEvans"
+        }
     ]
 });
 
